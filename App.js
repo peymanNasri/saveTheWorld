0470d03d0a72da22ee546d8f205ef906161b08b8
peymannasri <peymannasri1376@gmail.com>
2021-02-18 15:27:30 +0330

    stand by branch see what happened and edit app.js

--- App.js
+++ App.js
@@ -7,7 +7,9 @@ import HomePage from './pages/homepage/homepage.component';
 
 const HatsPage = () => (
   <div>
-    <h1>HATS PAGE </h1>
+  <h3>hell yeah </h3>
+	<p>im paragraph</p>
+	<h1>HATS PAGE </h1>
   </div>
 );
 
